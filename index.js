60248f4acb02e5ccf28c8fb1d1100d61dd2e71b7
archaudhari <81674825+archaudhari@users.noreply.github.com>
2023-02-09 21:25:21 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// Import stylesheets
-import './style.css';
+// ECMAScript 6 (ES6), also known as ECMAScript 2015, is the latest version of the ECMAScript standard and introduces many new features to the JavaScript programming language. Here are some of the most significant features introduced in ES6
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// let and const: New ways of declaring variables that offer improved scoping rules and better control over their lifetimes.
+
+const PI = 3.14; // Cannot be reassigned
+let x = 10; // Can be reassigned
+x = 20;
